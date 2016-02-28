fe65cdf87e8c63b85bc4a04fd3ec7e10fad140a4
mcle0341 <mcle0341@algonquinlive.com>
2016-02-28 11:34:06 -0500

    added space

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@ var counter;
 
 for (counter = 0; counter < 10; counter++) { 
 	document.write('m');
-
 }
 
 var name = '';
 
 while (!name) {
-name = prompt('Enter a letter:');
+	name = prompt('Enter a letter:')
 }
 
 document.write(name);
